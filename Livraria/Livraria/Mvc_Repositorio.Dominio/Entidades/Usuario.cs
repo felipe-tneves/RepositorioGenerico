a161163777ea3392c0e5a35dd2f69e1b5254660c
Felipe Tadeu Neves Santos <40270522859@info.local>
2019-09-20 11:28:33 -0300

    up2

---
+++ Livraria/Livraria/Mvc_Repositorio.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Mvc_Repositorio.Dominio.Entidades
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        [Required(ErrorMessage = "Informe o login do usuário.")]
+        [Display(Name = "Usuário")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Informe a senha do usuário.")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+        [Required(ErrorMessage = "Informe o email do usuário.")]
+        public string Email { get; set; }
+    }
+}
